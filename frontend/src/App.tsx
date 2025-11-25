59ec410f6db80692563a2e310d258a11e3c75978
Serban David <dudadud4418@gmail.com>
2025-11-25 17:27:32 +0200

    feature/TASK-1123 create auth page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,21 @@
-import './App.css'
-import { Button } from "@/components/ui/button"
+import './App.css';
+import { Button } from "@/components/ui/button";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AuthPage from './features/auth/AuthPage';
 
 function App() {
-
   return (
-    <>
-      <Button>
-      Click me
-      </Button>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/auth" element={<AuthPage />} />
+        <Route path="/" element={
+          <Button>
+            Click me
+          </Button>
+        } />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;

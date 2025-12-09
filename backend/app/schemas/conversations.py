5e110253a9a4a0ab5872d246feb295f41d010bfd
Dragos Vatra <Dragos.Vatra@endava.com>
2025-12-09 13:40:24 +0200

    Ruff fix

--- backend/app/schemas/conversations.py
+++ backend/app/schemas/conversations.py
@@ -1,5 +1,5 @@
 import uuid
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, ConfigDict
 from typing import Optional, List
 from datetime import datetime
 
--- backend/app/services/messages/send_message.py
+++ backend/app/services/messages/send_message.py
@@ -1,5 +1,5 @@
 import uuid
-from datetime import datetime
+#from datetime import datetime
 from sqlalchemy import update
 from sqlalchemy.ext.asyncio import AsyncSession
 
